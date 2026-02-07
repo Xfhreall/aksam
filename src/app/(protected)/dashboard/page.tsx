d620d73277bcc18c9f64684b430b28b58a5649ea
Xfhreall <farelrizqi377@gmail.com>
2026-02-07 18:24:57 +0700

    fix: correct import path for DashboardContainer in HomePage component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardContainer } from "@/features/home/containers/dashboard";
+import { DashboardContainer } from "@/features/home/containers/dashboard-container";
 
 export default function HomePage() {
   return <DashboardContainer />;
